d4ab936b477edaeb4c9ee998b09336c107c7e80a
SUGANYA2730 <102944961+SUGANYA2730@users.noreply.github.com>
2022-04-10 08:06:55 -0700

    Add files via upload

---
+++ practical/ArrayExample.java
@@ -0,0 +1,16 @@
+class ArrayExample{
+public static void main(String args[]){
+String[]names={"sugar","panda","donut","manshal"};
+System.out.println(names[3]);
+for(int i=0; i<names.length;i++)
+{
+System.out.println(names[i]);
+}
+System.out.println();
+int[]phones={112,213,4165,5628};
+for(int j=0;j<phones.length;j++)
+{
+System.out.println(phones[j]);
+}
+}
+}
---
+++ practical/WhileExample.java
@@ -0,0 +1,17 @@
+class WhileExample
+{
+public void show()
+{
+int a=50;
+while(a>=1)
+{
+System.out.println("Value is:" +a);
+a=a-5;
+}
+}
+public static void main(String args[])
+{
+WhileExample w=new WhileExample();
+w.show();
+}
+}
