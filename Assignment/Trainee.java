544c455127d5ea44fb292b20593f2c69f9656e76
SUGANYA2730 <102944961+SUGANYA2730@users.noreply.github.com>
2022-04-24 21:43:12 -0700

    Add files via upload

---
+++ Assignment/Trainee.java
@@ -0,0 +1,8 @@
+package Practical;
+public class Trainee extends Employee
+{
+public Trainee(long id, String Nmae, String Address, long Phone, double basicSalary)
+{
+super(id,Name,Address,Phone,basicSalary);
+}
+}
